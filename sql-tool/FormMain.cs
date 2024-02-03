b8a37b7efa575a7509cad46445db9b64b8f44a7f
Junio Cesar Ferreira <jcf_ssp@hotmail.com>
2024-02-03 10:51:50 -0300

    update - Projeto sql-tool .NET inicial

---
+++ sql-tool/FormMain.cs
@@ -0,0 +1,16 @@
+namespace sql_tool
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
