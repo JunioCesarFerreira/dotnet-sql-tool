abecda3fb9f125e0608493994cc832b2c9c879ad
Gabriel Nogueira <biel.snogueira48@hotmail.com>
2024-02-19 22:27:42 -0300

    dev - transformando função ColoringText para uso geral

--- sql-tool/FormShowMDFile.cs
+++ sql-tool/FormShowMDFile.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.IO;
 using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 
 namespace sql_tool
@@ -18,24 +18,27 @@ namespace sql_tool
         private void FormShowMDFile_Load(object sender, EventArgs e)
         {
             RichTextBox_MDFile.Text = File.ReadAllText(_pathFile);
-            ColoringText();
+            Colorizer.ColoringText(RichTextBox_MDFile);
         }
+    }
 
-        public void ColoringText()
+    public static class Colorizer
+    {
+        public static void ColoringText(RichTextBox richTextBox)
         {
-            string[] wordsToColorize = { "SELECT", "FROM", "WHERE", "INSERT", "INTO", "UPDATE", "DELETE", "CREATE", "ALTER", "DROP", "TABLE", "INDEX", "JOIN", "INNER", "OUTER", "LEFT", "RIGHT", "GROUP", "BY", "ORDER", "HAVING", "AND", "OR", "NOT", "IN", "BETWEEN", "LIKE", "AS", "DISTINCT", "COUNT", "SUM", "AVG", "MAX", "MIN", "ALL", "ANY", "EXISTS", "UNION", "INTERSECT", "EXCEPT", "CASE", "WHEN", "THEN", "ELSE", "END", "BEGIN", "COMMIT", "ROLLBACK", "TRANSACTION", "GRANT", "REVOKE", "USER", "DATABASE", "VIEW", "PROCEDURE", "TRIGGER", "CASCADE", "PRIMARY", "KEY", "FOREIGN", "CONSTRAINT", "SET", "VALUES"};
+            string[] wordsToColorize = { "SELECT", "FROM", "WHERE", "INSERT", "INTO", "UPDATE", "DELETE", "CREATE", "ALTER", "DROP", "TABLE", "INDEX", "JOIN", "INNER", "OUTER", "LEFT", "RIGHT", "GROUP", "BY", "ORDER", "HAVING", "AND", "OR", "NOT", "IN", "BETWEEN", "LIKE", "AS", "DISTINCT", "COUNT", "SUM", "AVG", "MAX", "MIN", "ALL", "ANY", "EXISTS", "UNION", "INTERSECT", "EXCEPT", "CASE", "WHEN", "THEN", "ELSE", "END", "BEGIN", "COMMIT", "ROLLBACK", "TRANSACTION", "GRANT", "REVOKE", "USER", "DATABASE", "VIEW", "PROCEDURE", "TRIGGER", "CASCADE", "PRIMARY", "KEY", "FOREIGN", "CONSTRAINT", "SET", "VALUES", "*" };
 
-            foreach (string  word in wordsToColorize)
+            foreach (string word in wordsToColorize)
             {
                 int startIndex = 0;
-                while(startIndex < RichTextBox_MDFile.TextLength)
+                while (startIndex < richTextBox.TextLength)
                 {
-                    int wordStartIndex = RichTextBox_MDFile.Find(word, startIndex, RichTextBoxFinds.WholeWord | RichTextBoxFinds.MatchCase);
+                    int wordStartIndex = richTextBox.Find(word, startIndex, RichTextBoxFinds.WholeWord | RichTextBoxFinds.MatchCase);
                     if (wordStartIndex != -1)
                     {
-                        RichTextBox_MDFile.Select(wordStartIndex, word.Length);
-                        RichTextBox_MDFile.SelectionColor = Color.Red;
-                        RichTextBox_MDFile.SelectionFont = new Font(RichTextBox_MDFile.Font, FontStyle.Bold);
+                        richTextBox.Select(wordStartIndex, word.Length);
+                        richTextBox.SelectionColor = Color.Red;
+                        richTextBox.SelectionFont = new Font(richTextBox.Font, FontStyle.Bold);
                         startIndex = wordStartIndex + word.Length;
                     }
                     else
